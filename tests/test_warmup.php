41315b853ceb6ce083f1e21d3f513485e1e55456
Christian Baltazar <christian.baltazar@helloprima.com>
2026-06-02 16:50:37 +0200

    Setup/testing (#1)
    * Add test boot files
    
    * Setup dev db
    
    * Create user entity and migration
    
    * Create first user test
    
    * Fix app styles

---
+++ tests/test_warmup.php
@@ -0,0 +1,51 @@
+<?php
+
+use App\Kernel;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\NullOutput;
+
+$kernel = new Kernel('test', true);
+$kernel->boot();
+$container = $kernel->getContainer();
+$application = new Application($kernel);
+$application->setAutoExit(false);
+
+$databasePath = $container->getParameter('kernel.project_dir') . '/var/test.db';
+
+if (file_exists($databasePath)) {
+    echo "// Removing test database...\n";
+    unlink($databasePath);
+}
+
+// Drop database if exists
+$application->run(new ArrayInput([
+    'command' => 'doctrine:database:drop',
+    '--env' => 'test',
+    '--force' => true,
+    '--if-exists' => true,
+]), new NullOutput());
+
+// Create database
+echo "// Recreating test database...\n";
+$application->run(new ArrayInput([
+    'command' => 'doctrine:database:create',
+    '--env' => 'test',
+]), new NullOutput());
+
+// Create schema
+echo "// Creating schema...\n";
+$application->run(new ArrayInput([
+    'command' => 'doctrine:schema:create',
+    '--env' => 'test',
+]), new NullOutput());
+
+// Load fixtures
+echo "// Loading fixtures...\n";
+$application->run(new ArrayInput([
+    'command' => 'doctrine:fixtures:load',
+    '--env' => 'test',
+    '--no-interaction' => true,
+]), new NullOutput());
+
+$kernel->shutdown();
