a1b811a43b75233a8b96ca14ddbd5773e72d27e9
Christian Baltazar <christian.baltazar@helloprima.com>
2026-06-11 10:26:26 +0200

    fixup! test(UserPAssword)

--- tests/Service/User/UserPasswordTest.php
+++ tests/Service/User/UserPasswordTest.php
@@ -25,7 +25,6 @@ class UserPasswordTest extends TestCase
         $hasher = $this->prophesize(UserPasswordHasherInterface::class);
         $hasher->hashPassword($user, Argument::any())->willReturn('hashed_password')->shouldBeCalled();
 
-
         $test = new UserPasswordService($hasher->reveal());
 
         $this->assertSame('hashed_password', $test($user, $plainPwd));
@@ -37,6 +36,33 @@ class UserPasswordTest extends TestCase
             ['test@mail.com', 'a12345'],
             ['test@mail.com', '12345x'],
             ['test@mail.com', 'abc123#'],
+        ];
+    }
+
+    #[Test]
+    #[DataProvider('provide_user_password_error')]
+    public function user_password_error(string $email, string $plainPwd): void
+    {
+        $user = new User();
+        $user->setEmail($email);
+
+        $hasher = $this->prophesize(UserPasswordHasherInterface::class);
+        $hasher->hashPassword($user, Argument::any())->shouldNotBeCalled();
+
+        $this->expectException(\LogicException::class);
+
+        $test = new UserPasswordService($hasher->reveal());
+
+        $test($user, $plainPwd);
+    }
+
+    public static function provide_user_password_error(): array
+    {
+        return [
+            ['test@mail.com', '123'],
+            ['test@mail.com', '123456'],
+            ['test@mail.com', 'test123'],
+            ['test@mail.com', 'qwerty'],
         ];
     }
 }
